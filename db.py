935026e506199d02bfbc5a2a4f3ba17869986fdb
1777777777777 <268452800+1777777777777@users.noreply.github.com>
2026-04-17 20:44:35 +0300

    added helper functions

--- db.py
+++ db.py
@@ -1,7 +1,26 @@
 import sqlite3
+from flask import g
+
 
 def get_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+    if "db" not in g:
+        conn = sqlite3.connect('database.db')
+        conn.execute("PRAGMA foreign_keys = ON")
+        conn.row_factory = sqlite3.Row
+        g.db = conn
 
+    return g.db
+
+def execute(sql, params=[]):
+    conn = get_connection()
+    result = conn.execute(sql, params)
+    conn.commit()
+    g.last_insert_id = result.lastrowid
+
+def query(sql, params=[]):
+    conn = get_connection()
+    result = conn.execute(sql, params).fetchall()
+    return result
+
+def last_insert_id():
+    return g.last_insert_id
